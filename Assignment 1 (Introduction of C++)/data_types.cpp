6de6934a217893ac6707dcb39cabbec05ae654f7
NASIR HUSAIN <104553289+Nasir-02@users.noreply.github.com>
2022-12-23 22:35:18 +0530

    Create data_types.cpp

---
+++ Assignment 1 (Introduction of C++)/data_types.cpp
@@ -0,0 +1,16 @@
+https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem?isFullScreen=true
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long int b;
+    char c;
+    float d;
+    double e;
+    cin>>a>>b>>c>>d>>e;
+    printf("%i\n%li\n%c\n%f\n%lf",a,b,c,d,e);
+    return 0;
+}
