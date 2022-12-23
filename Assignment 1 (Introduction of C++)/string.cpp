aa50babd1168a3e40a6edfbe7d40cb073a90af0b
NASIR HUSAIN <104553289+Nasir-02@users.noreply.github.com>
2022-12-23 22:37:15 +0530

    Create string.cpp

---
+++ Assignment 1 (Introduction of C++)/string.cpp
@@ -0,0 +1,17 @@
+https://www.hackerrank.com/challenges/c-tutorial-strings/problem?isFullScreen=true
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string a, b;
+    cin>>a>>b;
+    cout<<a.size()<<" "<<b.size()<<endl;;
+    cout<<a+b<<endl;
+    char temp = a[0];
+    a[0] = b[0];
+    b[0]= temp;
+    cout<<a<<" "<<b;
+    return 0;
+}
