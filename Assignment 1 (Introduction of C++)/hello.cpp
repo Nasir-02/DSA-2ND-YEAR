41d02eb45dfd3a47422d86a4509b276e6d9a4b9c
NASIR HUSAIN <104553289+Nasir-02@users.noreply.github.com>
2022-12-23 21:49:57 +0530

    Create hello.cpp

---
+++ Assignment 1 (Introduction of C++)/hello.cpp
@@ -0,0 +1,10 @@
+https://www.hackerrank.com/challenges/cpp-hello-world/problem?isFullScreen=true
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    cout<<"Hello, World!"<<endl;
+    return 0;
+}
