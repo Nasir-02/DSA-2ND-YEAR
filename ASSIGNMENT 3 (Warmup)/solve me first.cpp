556fb6ed2d83a06c2607cc6a84216eb04d48d787
NASIR HUSAIN <104553289+Nasir-02@users.noreply.github.com>
2023-02-04 17:03:08 +0530

    Create solve me first.cpp

---
+++ ASSIGNMENT 3 (Warmup)/solve me first.cpp
@@ -0,0 +1,12 @@
+https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    cin>>a>>b;
+    int sum=a+b;
+    cout<<sum;
+    return 0;
+}
