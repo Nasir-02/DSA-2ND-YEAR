a5fd95da7f9d7ee9ef4f888d780c62a576001278
NASIR HUSAIN <104553289+Nasir-02@users.noreply.github.com>
2023-02-04 17:37:53 +0530

    Create Time Conversion.cpp

---
+++ ASSIGNMENT 3 (Warmup)/Time Conversion.cpp
@@ -0,0 +1,30 @@
+https://www.hackerrank.com/challenges/time-conversion/problem?isFullScreen=true
+
+#include<bits/stdc++.h>
+using namespace std;
+#include <iostream>
+using namespace std;
+int main() {
+    string s,hr,min,sec;
+    cin>>s;
+    hr[0]=s[0];
+    hr[1]=s[1];
+    min[0]=s[3];
+    min[1]=s[4];
+    sec[0]=s[6];
+    sec[1]=s[7];
+    if(s[8]==80){
+        int num=stoi(hr);
+        hr=to_string(num+12);
+    }
+    else{
+        int num=stoi(hr);
+        hr=to_string(num+12);
+    }
+    if(hr[0]==50 && hr[1]==52){
+            hr[0]=48;
+            hr[1]=48;
+        }
+    cout<<hr[0]<<hr[1]<<":"<<min[0]<<min[1]<<":"<<sec[0]<<sec[1];
+    return 0;
+}
